8a295e6ef1232cacd3342ed573e745a304cd0574
redbeanzs <124220021+redbeanzs@users.noreply.github.com>
2024-04-12 14:43:52 -0400

    Create general_info.py

---
+++ general_info.py
@@ -0,0 +1,91 @@
+"""
+Masterdoc of themes, their associated terms, variations of said terms, and 
+the meaning of each term. 
+
+themes_terms_meanings is a dictionary with keys that are theme names 
+(ex: "stress and anxiety"). The value of each key is a list of dictionaries.
+Each item/dictionary represents one common dream term. Each item is a 
+dictionary with keys: "term", "variations", "meanings".
+"""
+
+themes_terms_meanings = {
+    "stress and anxiety": 
+    [{"term": "teeth","variations": ["teeth", "tooth", "mouth"], 
+     "meanings": ["stress", "inability to process or digest information"]},
+    {"term": "being chased","variations": ["being chased", "chase", "race", 
+    "hunted"], "meanings": ["avoidance", "unresolved problems"]},
+    {"term": "falling","variations": ["falling", "fell", "fallen"], 
+     "meanings": ["insecurity", "lack of support"]},
+    {"term": "being late","variations": ["being late", "lateness"],
+     "meanings": ["anxiety about expectations", "fear of judgment"]},
+    {"term": "fire","variations": ["fire", "fires", "flame", "flames", 
+    "burn", "burnt"] ,"meanings": ["uncontrolled anger", "destructive emotions"]},
+    {"term":"earthquake","variations": ["earthquake", "earthquakes"],
+     "meanings": ["upheaval", "instability in life"]},
+    {"term": "storms" ,"variations":["storms", "storm", "hurricane", "hurricanes"] 
+     ,"meanings": ["turmoil", "overwhelming challenges"]},
+    {"term":"snakes","variations":["snakes", "snake", "python", "pythons", 
+    "cobra", "cobras"] ,"meanings": ["hidden threats", "complex issues"]}],
+    
+    "transitions and changes":
+    [{"term": "life changes" ,"variations": ["life changes", "change"],
+     "meanings": ["transition", "entering a new phase"]},
+    {"term":"roads","variations": ["road", "roads", "pavement", "highway", "street"] 
+     ,"meanings": ["life journey", "choices ahead"]},
+    {"term": "crossroads","variations": ["crossroads", "cross-roads"]
+        ,"meanings": ["decision-making", "life-changing choices"]},
+    {"term": "moving houses" ,"variations": ["moving houses", "moving house", 
+    "moving"],"meanings": ["major life changes", "identity shifts"]}],
+    
+    "positive emotional states": 
+    [{"term": "flying","variations": ["flying", "fly", "soar", "soaring"] ,
+     "meanings": ["freedom", "release from constraints"]},
+    {"term": "water","variations": ["water", "waterfall", "shower", "rain"],
+     "meanings": ["cleansing", "emotional refreshment"]},
+    {"term": "being happy","variations": ["contentment", "peace"],
+     "meanings":["being happy", "happiness"]},
+    {"term": "celebration","variations": ["celebrations", "celebration"],
+     "meanings": ["joy", "achievements"]}],
+    
+    "needs and wants":
+    [{"term": "food","variations": ["food", "meal", "eat", "eating"] ,
+     "meanings": ["nourishment", "needs being met"]},
+    {"term": "traveling","variations":["traveling", "travelling", "travel"] ,
+     "meanings": ["need for escape", "exploration"]},
+    {"term":"climbing","variations":["climbing", "climb"] ,
+     "meanings": ["ambition", "striving to overcome challenges"]}],
+    
+    "relationships":
+    [{"term":"weddings" ,"variations": ["weddings", "wedding", "marraige", "partner",
+    "wife", "husband", "wives", "husbands"] ,
+     "meanings":["union or commitment", "relationship goals"]},
+    {"term": "parties","variations": ["parties", "party"] ,
+     "meanings": ["social desires", "need for belonging"]},
+    {"term": "being naked","variations": ["being naked", "naked", "nude"],
+     "meanings": ["vulnerability", "truth"]}],
+    
+    "reflection" :
+    [{"term":"mirrors" ,"variations": ["mirrors", "mirror"],
+     "meanings":["self-image", "truth about oneself"]},
+    {"term": "masks","variations":["masks", "mask"],
+     "meanings":["concealment", "hidden emotions"]}],
+    
+    "fears": 
+    [{"term":"heights","variations":["heights", "height"],
+     "meanings": ["fear of success or failure"]},
+    {"term":"darkness","variations":["darkness", "the dark"],
+     "meanings": ["fear of the unknown", "hidden issues"]},
+    {"term": "monsters","variations": ["monsters", "monster"],
+     "meanings": ["fears", "repressed emotions"]}],
+    
+    "spiritual insights": 
+    [{"term":"death","variations":["death", "died", "dead", "killed"],
+     "meanings": ["end of something", "change needed "]},
+    {"term": "flying in space","variations":["flying in space", 
+    "space flight", "space flying"] ,"meanings": ["seeking freedom", "need an adventure"]},
+    {"term": "being lost","variations": ["lost"],"meanings": ["feeling Lost", "seeking purpose"]}]}
+    
+
+# https://www.dreammoods.com/
+# https://www.dreambible.com/search.php?q=Flying
+# https://www.dreamdictionary.org/
